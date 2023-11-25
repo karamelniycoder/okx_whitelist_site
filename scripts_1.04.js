bf0528035a2fc039f4770ebbaaf1650740550c06
karamelniycoder <109586521+karamelniycoder@users.noreply.github.com>
2023-11-25 21:55:41 +0700

    add bybit code

--- scripts_1.04.js
+++ scripts_1.04.js
@@ -6,7 +6,14 @@ function splitTextOnPaste(event, element_id) {
 
     // ELEMENT
 
-    const lines = pastedText.split('\n').slice(0, 20);
+    let lines;
+    if (["addresses", "names"].includes(element_id)) {
+        lines = pastedText.split('\n').slice(0, 20);
+    } else if (element_id === "bybit_label") {
+        lines = pastedText.split('\n').slice(0, 1);
+    } else {
+        lines = pastedText.split('\n');
+    }
 
     const formattedLines = lines.map((line) => {
         return line.trim();
@@ -16,7 +23,12 @@ function splitTextOnPaste(event, element_id) {
     textareaElement.value = formattedLines.join('\n');
 
     // CODE
-    codeOnChange()
+    if (element_id === "addresses") {
+        codeOnChange();
+    } else {
+        bybitOnChange();
+    }
+
 
 }
 
@@ -127,19 +139,87 @@ addWallets();
 }
 
 
-function copyTextToClipboard() {
-    const textToCopy = document.getElementById('code').value;
-    if (textToCopy) {
-        navigator.clipboard.writeText(textToCopy)
+function bybitOnChange() {
+    const addressesText = document.getElementById('addresses_two').value;
+    const addressLabel = document.getElementById('bybit_label').value;
 
-        const toast = document.getElementsByClassName('toast_default')[0]
-        const code = document.getElementById('code')
+    const formattedText = `const delay = e => new Promise((t => setTimeout(t, e)));
+
+function clickElement(e) {
+    ["mousedown", "mouseup", "click"].forEach((t => {
+        const n = new MouseEvent(t, {
+            bubbles: !0,
+            cancelable: !0,
+            view: window
+        });
+        e.dispatchEvent(n)
+    }))
+}
+
+first_checkbox = document.querySelector("#dynamic_form_nest_item > div:nth-child(4) > div > div > div > div > div:nth-child(1) > label > span.flex.justify-center.items-center.relative > span > input");
+second_checkbox = document.querySelector("#dynamic_form_nest_item > div:nth-child(4) > div > div > div > div > div:nth-child(2) > label > span.flex.justify-center.items-center.relative > span > input");
+
+if (!first_checkbox.checked) {clickElement(first_checkbox)}
+if (!second_checkbox.checked) {clickElement(second_checkbox)}
+
+
+function setValue(e, t) {
+    const n = Object.getOwnPropertyDescriptor(e, "value").set,
+        c = Object.getPrototypeOf(e),
+        o = Object.getOwnPropertyDescriptor(c, "value").set;
+    n && n !== o ? o.call(e, t) : n.call(e, t), e.dispatchEvent(new Event("input", {
+        bubbles: !0
+    }))
+}
+window.addAddresses = async function(e, t, n = 1) {
+    await delay(1e3);
+    for (let t = 0; t < e.length - 1; t++) {
+        document.querySelector("#dynamic_form_nest_item .money__address-batch-form-action-btn").click(), await delay(100)
+    }
+    const c = Array.from(document.querySelectorAll("#dynamic_form_nest_item > .ant-space"));
+    for (let o = 0; o < c.length; o++) {
+        const l = c[o],
+            a = e[o];
+        clickElement(l.querySelector(".ant-space-item:nth-child(2) .ant-form-item-control.css-29t93j > div > div > div > div")), await delay(100);
+        const i = Array.from(document.querySelectorAll(".ant-select-dropdown"))[o];
+        setValue(i.querySelector("input"), "Linea"), await delay(500);
+        clickElement(i.querySelector('div[title="LINEA"]')), await delay(500);
+        setValue(l.querySelector(".ant-space-item:nth-child(3) input"), a);
+        setValue(l.querySelector(".ant-space-item:nth-child(5) input"), \`\${t}\${Number(n) + Number(o)}\`)
+    }
+}, window.generateFunctions = function(e, t, n = 1) {
+    const c = e.trim().split("\\n").map((e => e.trim()));
+    let o = 1;
+    for (let e = 0; e < c.length; e += 5) {
+        const l = c.slice(e, e + 5);
+        window[\`add\${o}\`] = (e => () => window.addAddresses(l, t, e))(n), n += l.length, console.log(\`Generated add\${o}()\`), o++
+    }
+};
+
+generateFunctions(\`${addressesText}\`, "${addressLabel}", 1)
+`;
+    document.getElementById('code_two').value = formattedText;
+
+}
+
+
+function copyTextToClipboard(code_id) {
+    const textToCopy = document.getElementById(code_id);
+    if (textToCopy.value) {
+        navigator.clipboard.writeText(textToCopy.value)
+
+        let toast;
+        if (code_id === 'code') {
+            toast = document.getElementsByClassName('toast_default')[0]
+        } else if (code_id === 'code_two') {
+            toast = document.getElementsByClassName('toast_default')[1]
+        }
         toast.classList.add("toast_show")
-        code.classList.add('colored_textarea')
+        textToCopy.classList.add('colored_textarea')
         setTimeout(() => {
             {
                 toast.classList.remove("toast_show");
-                code.classList.remove('colored_textarea')
+                textToCopy.classList.remove('colored_textarea')
             }
         }, 500)
 
@@ -162,7 +242,36 @@ function calculateMarqueeContent() {
 }
 
 
+function showFieldsOKX() {
+    var fields_okx = document.getElementById("okx_fields");
+    var fields_bybit = document.getElementById("bybit_fields");
+    var okx_button = document.getElementsByClassName("okx_button")[0];
+    var bybit_button = document.getElementsByClassName("bybit_button")[0];
+
+    okx_button.style.opacity = 0.5;
+    bybit_button.style.opacity = 1;
+
+    fields_okx.style.display = "block";
+    fields_bybit.style.display = "none";
+}
+
+
+function showFieldsBybit() {
+    var fields_okx = document.getElementById("okx_fields");
+    var fields_bybit = document.getElementById("bybit_fields");
+    var okx_button = document.getElementsByClassName("okx_button")[0];
+    var bybit_button = document.getElementsByClassName("bybit_button")[0];
+
+    okx_button.style.opacity = 1;
+    bybit_button.style.opacity = 0.5;
+
+    fields_okx.style.display = "none";
+    fields_bybit.style.display = "block";
+}
+
+
 window.addEventListener('load', calculateMarqueeContent);
+window.addEventListener('load', showFieldsOKX);
 window.addEventListener('resize', calculateMarqueeContent);
 
 window.addEventListener('load', function () {
@@ -171,6 +280,13 @@ window.addEventListener('load', function () {
     lineNumbers.forEach(function (lineNumber) {
         for (let i = 1; i <= 20; i++) {
             lineNumber.innerHTML += i + '<br>';
+        }
+    });
+
+    const lineNumbers_5 = document.querySelectorAll('.line-numbers_5');
+    lineNumbers_5.forEach(function (lineNumber_5) {
+        for (let i = 1; i <= 5; i++) {
+            lineNumber_5.innerHTML += i + '<br>';
         }
     });
 });
